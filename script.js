93a82153060233cc307530487352923083da177d
OLUWATOBILOBA <rapzter58@gmail.com>
2021-11-02 00:49:32 +0100

    new changes

--- script.js
+++ script.js
@@ -3,23 +3,9 @@ const burgerButton = document.querySelector('.burger-button');
 burgerButton.addEventListener('click', ()=>{
     document.body.classList.toggle('nav-open');
 });
-const circle2 = document.querySelector('.circle-2');
-
-const circle1 = document.querySelector('.circle-1');
 
 
-setInterval(()=>{
-    circle2.style.transform = "scale(1.0857)";
-},500);
-setInterval(()=>{
-    circle2.style.transform = "scale(1)";
-},800);
-setInterval(()=>{
-    circle1.style.transform = "scale(1.1)";
-},600);
-setInterval(()=>{
-    circle1.style.transform = "scale(1)";
-},800);
+
 
 const navLinks= document.querySelectorAll('.nav-links');
 
@@ -29,19 +15,24 @@ navLinks.forEach(link=>{
     });
 });
 
- 
+const fav = ["gaming","art","sport"]
+document.querySelector('fanof');
+let Floater = document.querySelector('.float-text');
+const hello = "hello world";
+let paddedWord ="";
 
-/*var isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-window.addEventListener('scroll', function (event) {
-	if (isInViewport(skills)) {
+    for (let i=0; i<hello.length; i++){
+        setTimeout(()=>{
+            Floater.innerHTML +=hello.charAt(i);
+            console.log(Floater.innerHTML);
+        },1000)
         
-	}
-}, false);*/
+    }
+
+
+
+//setTimeout(()=>{
+//   
+//   Floater.innerHTML = " ";
+//,1000)
+
